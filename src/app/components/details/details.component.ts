b46a5784c95c7e03669d5b8398c22a13c5a7429d
montoyafranco <francomontoya9810@gmail.com>
2022-09-11 11:43:00 -0300

    making changes trying routes and paths 2

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-details',
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DetailsComponent implements OnInit {
 
   constructor() { }
+  @Input() postSelected? : any
 
   ngOnInit(): void {
   }
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -2,59 +2,59 @@ import { Component, OnInit } from '@angular/core';
 import { RequestService } from 'src/app/services/request.service';
 import { Post } from 'src/app/services/models';
 import { CreatePostCommand } from 'src/app/services/models';
-import { WebSocketSubject } from 'rxjs/webSocket'; 
+import { WebSocketSubject } from 'rxjs/webSocket';
 import { SocketService } from 'src/app/services/socket/socket.service';
-
 
 @Component({
   selector: 'app-main', //santi dice app-post.page
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
- 
+  posts?: Post[];
 
-  posts? : Post[];
+  newTitle: string = '';
+  newAuthor: string = '';
+  //traigo el socketmangaer
+  socketManager?: WebSocketSubject<Post>;
 
-  newTitle : string = "";
-  newAuthor: string = "";  
-   //traigo el socketmangaer
-   socketManager?:WebSocketSubject<Post>;
-
-  constructor(private request : RequestService,    private socket:SocketService) { }
+  constructor(private request: RequestService, private socket: SocketService) {}
 
   ngOnInit(): void {
-    this.bringPosts()
-    this.connectToMainSpace()
+    //declaro que siempre que inicie me traiga todos los post y se conecte al socket
+    this.bringPosts();
+    this.connectToMainSpace();
   }
-  bringPosts(){ //getallpost
-    this.request.bringAllPost().subscribe(posts =>{
-      this.posts = posts
-    }
-      )
+  bringPosts() {
+    //getallpost .  request esta inicializado en constructor request:RquestService , depndecy injection
+    this.request.bringAllPost().subscribe((posts) => {
+      this.posts = posts;
+    });
   }
-  submitPost(){
-    const newCommand : CreatePostCommand =
-    {
-      postId : Math.floor(Math.random() * 100000 ).toString(),
-      title : this.newTitle,
-      author : this.newAuthor
+  submitPost() {
+    //recojo lo entrado con ngmodel newtitle y autor y lo paso aca y al final lo vuelvo a ""
+    const newCommand: CreatePostCommand = {
+      postId: Math.floor(Math.random() * 100000).toString(),
+      title: this.newTitle,
+      author: this.newAuthor,
+    };
 
-    }
-  this.request.CreaPostAction(newCommand).subscribe()
-  this.newTitle = ""
-  this.newAuthor = ""
+    this.request.CreaPostAction(newCommand).subscribe();
 
+    // lo de abajo lo vuelve a espacio en blanco
+
+    this.newTitle = '';
+
+    this.newAuthor = '';
   }
-  connectToMainSpace(){
-    this.socketManager = this.socket.connectAll()
-    this.socketManager.subscribe((message) =>{      this.posts?.unshift(message)
-    })
+  connectToMainSpace() {
+    this.socketManager = this.socket.connectAll();
+    this.socketManager.subscribe((postNuevo) => {
+      this.posts?.unshift(postNuevo);
+      //unshift lo que hace es que lo agrega al inicio del array 
+    });
   }
-  closeSocket(){
+  closeSocket() {
     this.socketManager?.complete();
   }
-  
-  
-
 }
